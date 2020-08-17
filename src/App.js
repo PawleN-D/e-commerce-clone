54066204f93d0538ea193d855cf28d7cde51aad1
PawleN-D <sirpawle@gmail.com>
2020-08-17 02:18:10 +0200

    added header component and styling script

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+          <Route path="/">
+          <Header />
+            <h1>Home Page</h1> 
+          </Route>
+        </Switch>
+      </div>
+    </Router>
 
-    </div>
   );
 }
 
